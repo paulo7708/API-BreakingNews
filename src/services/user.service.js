83816bab6df643de6f12fad078230fc3d8860eed
Paulo7708 <dawnbreakerph@gmail.com>
2023-03-28 13:12:03 -0300

    JWT em uma API e Readme

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,13 @@
 import User from "../models/User.js";
 
 const createService = (body) => User.create(body);
+/*create metodo do moongose */
 
 const findAllService = () => User.find();
+/*find metodo do moongose */
 
 const findByIdService = (id) => User.findById(id);
+/*findById metodo do moongose */
 
 const updateService = (
   id,
