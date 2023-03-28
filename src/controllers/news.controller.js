f1007b12dad09a8d39076cedf805aaad122d5220
Paulo7708 <dawnbreakerph@gmail.com>
2023-03-28 15:11:12 -0300

    feat: model news, create news

---
+++ src/controllers/news.controller.js
@@ -0,0 +1,38 @@
+import { createService, findAllService } from "../services/news.service.js";
+
+const create = async (req, res) => {
+  try {
+    const { title, text, banner } = req.body;
+
+    if (!title || !banner || !text) {
+      res.status(500).send({ message: "Submit all fields for registration!" });
+    }
+
+    await createService({
+      title,
+      text,
+      banner,
+      user: { _id:"64121cc90c72aee0b79cf51d"},
+    });
+
+    res.send(201);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+const findAll = async (req, res) => {
+  try {
+    const news = await findAllService();
+
+    if (news.lenght === 0) {
+      return res.status(400).send({ message: "Não há news registrado" });
+    }
+    res.send(news);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+  
+};
+
+export { create, findAll };
