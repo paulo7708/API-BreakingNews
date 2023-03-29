b2759696b9d34462c62d747b6f748fe486e4ae4e
Paulo7708 <dawnbreakerph@gmail.com>
2023-03-29 11:30:37 -0300

    route with JWT authentication

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -2,6 +2,23 @@ import { createService, findAllService } from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
+    const { authorization } = req.headers;
+    
+    if(!authorization){
+      return res.send(401);
+    }
+
+    const parts = authorization.split(" ")
+    const [ schema, token ] = parts
+
+    if(parts.lenght !== 2){
+      return res.send(401);
+    }
+
+    if(schema !== "Bearer"){
+      return res.send(401);
+    }
+
     const { title, text, banner } = req.body;
 
     if (!title || !banner || !text) {
