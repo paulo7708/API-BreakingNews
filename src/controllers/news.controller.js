dc27bf35f91281539b449f158109bd340e0e4e42
Paulo7708 <dawnbreakerph@gmail.com>
2023-03-29 17:00:39 -0300

    21 - Pagination of data in an API in Express

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,7 +1,11 @@
-import { createService, findAllService } from "../services/news.service.js";
+import {
+  createService,
+  findAllService,
+  countNews,
+} from "../services/news.service.js";
 
 const create = async (req, res) => {
-  try {    
+  try {
     const { title, text, banner } = req.body;
 
     if (!title || !banner || !text) {
@@ -23,16 +27,56 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
   try {
-    const news = await findAllService();
+    let { limit, offset } = req.query;
 
-    if (news.lenght === 0) {
-      return res.status(400).send({ message: "Não há news registrado" });
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
     }
-    res.send(news);
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    console.log(limit, offset);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
+    
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
-  
 };
 
 export { create, findAll };
