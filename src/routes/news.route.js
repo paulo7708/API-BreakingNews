f9646561b6508e5ec15b20b05148658fdda13112
Paulo7708 <dawnbreakerph@gmail.com>
2023-03-30 14:27:17 -0300

    25 - News by user

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 const router = Router();
 
-import { create, findAll, topNews, findById, searchByTitle } from '../controllers/news.controller.js'
+import { create, findAll, topNews, findById, searchByTitle, byUser } from '../controllers/news.controller.js'
 import { authMiddleware } from "../middlewares/auth.meddleware.js";
 
 router.post("/", authMiddleware, create)
 router.get("/", findAll)
 router.get("/top", topNews)
 router.get("/search", searchByTitle)
+router.get("/byUser", authMiddleware, byUser)
+
 router.get("/:id", findById)
 
 export default router;
